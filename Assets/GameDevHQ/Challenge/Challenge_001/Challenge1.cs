4a07337e0c74745fc56c9e6791c20410da444407
Samir Georgy <57589276+samirgeorgy@users.noreply.github.com>
2020-02-02 08:38:36 +0200

    Challange 1 done

---
+++ Assets/GameDevHQ/Challenge/Challenge_001/Challenge1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Challenge1 : MonoBehaviour
+{
+    #region Private Variables
+
+    [SerializeField] private int _age;
+    [SerializeField] private char _gender;
+    [SerializeField] private string _location;
+    [SerializeField] private Text _infoText;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _age = 0;
+        _gender = '-';
+        _location = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _infoText.text = _age.ToString() + " " + _gender + " " + _location;
+        }
+    }
+}
